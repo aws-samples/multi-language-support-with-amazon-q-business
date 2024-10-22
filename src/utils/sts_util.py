63d4459a715c415e2fcc126b34de0763395ff161
Ahmed Mohamed <ahmoe@amazon.com>
2024-10-22 13:01:38 -0400

    security fixes

--- src/utils/sts_util.py
+++ src/utils/sts_util.py
@@ -6,7 +6,7 @@ def assume_role_with_token(iam_token):
     """
     Assume IAM role using the IAM OIDC idToken.
     """
-    decoded_token = jwt.decode(iam_token, options={"verify_signature": False})
+    decoded_token = jwt.decode(iam_token, options={"verify_signature": True})
     sts_client = boto3.client("sts", region_name=st.session_state.REGION)
     response = sts_client.assume_role(
         RoleArn=st.session_state.IAM_ROLE,
